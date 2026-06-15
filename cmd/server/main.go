757418499c0b18945d23694292e2e91a47f15fed
cy4strii <arinasushko@yandex.ru>
2026-06-15 13:37:41 +0300

    edit ports (#11)
    * edit ports
    
    * Update main.go
    
    ---------
    
    Co-authored-by: justchelovk <misha.freidson@gmail.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -55,7 +55,7 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:8080"},
+		AllowedOrigins:   []string{"http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8080", "http://127.0.0.1:8081", "http://localhost:8081"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
 		AllowCredentials: true,
