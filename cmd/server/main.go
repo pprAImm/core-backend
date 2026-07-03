fa41cbdf87a4233905ac4ee9c9df7a2cfeb2eb81
cy4strii <arinasushko@yandex.ru>
2026-07-03 23:14:22 +0500

    add praimm.ru to CORS allowed origins

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -72,7 +72,7 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8080", "http://127.0.0.1:8081", "http://localhost:8081"},
+		AllowedOrigins:   []string{"https://praimm.ru", "http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8080", "http://127.0.0.1:8081", "http://localhost:8081"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
 		AllowCredentials: true,
